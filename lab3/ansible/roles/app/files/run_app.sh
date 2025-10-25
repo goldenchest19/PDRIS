ec4a03af5682824e072fbfd3d19f68b37489f100
goldenchest19 <kirya-ignatev-2016@mail.ru>
2025-10-25 16:04:03 +0300

    Доработка конфигураций, чтобы деплоилось на 2 пустых контейнера

--- lab3/ansible/roles/app/files/run_app.sh
+++ lab3/ansible/roles/app/files/run_app.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Проверяем доступность Docker..."
+echo "Проверяем доступность Docker."
 if ! docker info >/dev/null 2>&1; then
     echo "Docker daemon не доступен! Проверьте, что Docker запущен."
     exit 1
@@ -11,11 +11,11 @@ fi
 APP_DIR="/home/admin/app"
 cd "$APP_DIR"
 
-echo "Останавливаем старые контейнеры (если есть)..."
-docker-compose down || true
+echo "Останавливаем старые контейнеры"
+docker compose down || true
 
-echo "Запускаем контейнеры..."
-docker-compose up -d
+echo "Запускаем контейнеры"
+docker compose up -d
 
 echo "Приложение запущено!"
 docker ps
